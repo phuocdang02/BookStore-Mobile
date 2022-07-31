791a4805c0efebe1533813845b5e34430a157149
PhuocDeng <haphuocdang@gmail.com>
2022-07-31 23:55:30 +0700

    Nothing

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import Login from './components/LoginComponent';
 
 class App extends Component {
   render() {
     return (
-      <Main />
+      //<Main />
+      <Login/>
     );
   }
 }
